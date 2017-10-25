30868312d56956753697f117cfb37447791480ee
sdjcse <sdjcse1@gmail.com>
2017-10-24 19:19:14 -0700

    added visualization code

--- Assignment3/performPCAandPlot.m
+++ Assignment3/performPCAandPlot.m
@@ -10,16 +10,23 @@ function[] = performPCAandPlot(trainingData)
     disp('Corresponding Eigen Vectors (columnwise) are');
     eigenVectors = fliplr(eigenVectors);
     disp(eigenVectors);
+    transformed = trainingData(:,2:8)*eigenVectors;
+    transformed = transformed(:,1:3);
+    scatter3(transformed(1:1500,1),transformed(1:1500,2),transformed(1:1500,3),5,[0,0,1]);
+    hold on;
+    scatter3(transformed(1501:3000,1),transformed(1501:3000,2),transformed(1501:3000,3),5,[1,0,0]);
+    title('Transformed Data using eigen vectors');
+    pause(5);
+    hold off;
     
 %     Plot
-    labels = {'Gyro-X NWAMP', 'Gyro-Y NWAMP', 'Gyro-Z NWAMP', 'Orientation-X Variance', 'Orientation-Y Variance', 'Orientation-Z Variance', 'Orientation-w Variance'};
-    sizeEig =size(eigenVectors);
-    for i=1:sizeEig(2)
-        bar(eigenVectors(:,i)');
-        set(gca,'xticklabel',labels);
-        imageName = strcat('Task3PlotImages/','EigenVect',num2str(i),'.png');
-        saveas(gcf, imageName);
-    end
+    labels = {'GXW', 'GYW', 'GZW', 'OXV', 'OYV', 'OZV', 'OwV'};
+%    sizeEig =size(eigenVectors);
+%    for i=1:sizeEig(2)
+%        bar(eigenVectors(:,i)');
+%        set(gca,'xticklabel',labels);
+%        imageName = strcat('Task3PlotImages/','EigenVect',num2str(i),'.png');
+%        saveas(gcf, imageName);
 %     Select 3 Vectors
     noOfDims = 3;
     reducedEigenVectors = eigenVectors(:,1:noOfDims);
