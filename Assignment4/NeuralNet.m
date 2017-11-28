90cea4c738b477af0742d95bf0d20ddbfcc92815
gvamsi <vamsikrishnag@Vamsis-MacBook-Pro.local>
2017-11-28 01:04:45 -0700

    Nerual net fix0.1

--- Assignment4/NeuralNet.m
+++ Assignment4/NeuralNet.m
@@ -6,14 +6,16 @@ trainlabel = train(:,end);
 disp("Begin training");
 disp("Creating a 2 layered neural net with one hidden layer");
 net = feedforwardnet(10);
-net = configure(net,traindata);
-res = train(net,traindata);
+%net = configure(net,traindata);
+disp(traindata');
+disp(trainlabel');
+[net,tr] = train(net,traindata',trainlabel');
 disp("Training done");
 
 disp("Being test");
 testdata = test(:,1:end-1);
 actuallabel = test(:,end);
-[predictedlabel,scores] = predict(model,testdata);
+[predictedlabel,scores] = predict(net,testdata);
 [precision,recall,f1,auc] = calculateAccuracy(actuallabel,predictedlabel,scores);
 disp("End test");
 end
